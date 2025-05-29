0c9b2faf90707578ae3e7fbcbbea8d74cc92bbf5
Adam Allison <allisonadam81@gmail.com>
2025-05-29 10:53:30 -0700

    initial fixes

--- Vanilla_Example/metro.config.js
+++ Vanilla_Example/metro.config.js
@@ -7,9 +7,9 @@ const path = require('path');
  * @type {import('@react-native/metro-config').MetroConfig}
  */
 const extraNodeModules = {
-  '@allisonadam81/react-native-super-clusters': path.resolve(__dirname, '..'),
+  '@allisonadam81/react-native-super-clusters': path.resolve(__dirname, '../lib'),
 };
-const watchFolders = [path.resolve(__dirname, '..')];
+const watchFolders = [path.resolve(__dirname, '../lib')];
 const config = { resolver: { extraNodeModules }, watchFolders };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
