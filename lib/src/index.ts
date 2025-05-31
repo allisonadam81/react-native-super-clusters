bc72bd1fcd2e1c3d3f2a9dee24c6c8ec10816c4f
Adam Allison <allisonadam81@gmail.com>
2025-05-31 07:56:00 -0700

    changed export/imports to optional named

---
+++ lib/src/index.ts
@@ -0,0 +1,8 @@
+export { default } from './ClusteredMapView';
+
+export { default as ClusteredMapView } from './ClusteredMapView';
+export { default as ClusteredMapMarker } from './ClusteredMarker';
+
+export * from './ClusteredMapView';
+export * from './ClusteredMarker';
+export * from './utils';
